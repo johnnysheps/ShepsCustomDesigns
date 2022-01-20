2a7d8343d63aad327d2aead2f61ddd9de7ef7b50
johnnysheps <johnsheppard281@gmail.com>
2022-01-20 13:52:23 -0500

    update menu items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+
+
+const TShirtsPage = () => (
+  <div>
+    <h1>T-SHIRTS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/tshirts' component={TShirtsPage}/>
+      </Switch>
     </div>
   );
 }

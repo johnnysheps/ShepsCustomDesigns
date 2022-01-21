f448f93242b47d17720989a4b2eac13da25e6fd8
johnnysheps <johnsheppard281@gmail.com>
2022-01-21 18:28:08 -0500

    updating app with Shop Page and Collection Preview

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,177 @@
+const SHOP_DATA = [
+    {
+      id: 1,
+      title: 'T-Shirts',
+      routeName: 'tshirts',
+      items: [
+        {
+          id: 1,
+          name: 'Probably worth the Google',
+          imageUrl: '/assets/img/tshirts/google.png',
+          price: 25
+        },
+        {
+          id: 2,
+          name: 'People...what a bunch of bastards',
+          imageUrl: '/assets/img/tshirts/peopleblack.png',
+          price: 25
+        },
+        {
+          id: 3,
+          name: 'What Doesnt Kill You...',
+          imageUrl: '/assets/img/tshirts/whatdoesntkillyou.png',
+          price: 25
+        },
+        {
+          id: 4,
+          name: 'xUSD',
+          imageUrl: '/assets/img/tshirts/xusdshirt.png',
+          price: 25
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: 'Hoodies',
+      routeName: 'hoodies',
+      items: [
+        {
+          id: 1,
+          name: 'xUSD',
+          imageUrl: '/assets/img/hoodies/xusdhoodie.png',
+          price: 35
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: 'Decals',
+      routeName: 'decals',
+      items: [
+        {
+          id: 1,
+          name: 'Binance',
+          imageUrl: '/assets/img/decals/bnb.png',
+          price: 5
+        },
+        {
+          id: 2,
+          name: 'Crypto.com',
+          imageUrl: '/assets/img/decals/cro.png',
+          price: 5
+        },
+        {
+          id: 3,
+          name: 'Google Dinosaur',
+          imageUrl: '/assets/img/decals/GoogleDino.png',
+          price: 5
+        },
+        {
+          id: 4,
+          name: 'Safemoon',
+          imageUrl: '/assets/img/decals/safemoon.png',
+          price: 5
+        },
+        {
+          id: 5,
+          name: 'xSurge',
+          imageUrl: '/assets/img/decals/xsurge.png',
+          price: 5
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      routeName: 'womens',
+      items: [
+        {
+          id: 1,
+          name: 'Blue Tanktop',
+          imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
+          price: 25
+        },
+        {
+          id: 2,
+          name: 'Floral Blouse',
+          imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
+          price: 20
+        },
+        {
+          id: 3,
+          name: 'Floral Dress',
+          imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
+          price: 80
+        },
+        {
+          id: 4,
+          name: 'Red Dots Dress',
+          imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
+          price: 80
+        },
+        {
+          id: 5,
+          name: 'Striped Sweater',
+          imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
+          price: 45
+        },
+        {
+          id: 6,
+          name: 'Yellow Track Suit',
+          imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
+          price: 135
+        },
+        {
+          id: 7,
+          name: 'White Blouse',
+          imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
+          price: 20
+        }
+      ]
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      routeName: 'mens',
+      items: [
+        {
+          id: 1,
+          name: 'Camo Down Vest',
+          imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+          price: 325
+        },
+        {
+          id: 2,
+          name: 'Floral T-shirt',
+          imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+          price: 20
+        },
+        {
+          id: 3,
+          name: 'Black & White Longsleeve',
+          imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+          price: 25
+        },
+        {
+          id: 4,
+          name: 'Pink T-shirt',
+          imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+          price: 25
+        },
+        {
+          id: 5,
+          name: 'Jean Long Sleeve',
+          imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
+          price: 40
+        },
+        {
+          id: 6,
+          name: 'Burgundy T-shirt',
+          imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+          price: 25
+        }
+      ]
+    }
+  ]
+
+  export default SHOP_DATA;
